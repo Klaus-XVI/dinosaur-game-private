4218df73a531fc3bd5c081d0ea937ce5b7344417
Ann-Sophie <annso>
2023-04-06 22:36:53 +0200

    upload kaboom project

---
+++ kaboom-barebones-project/level1.js
@@ -0,0 +1,11 @@
+// const clickableImage = document.getElementById("clickableImage");
+// let clickCount = 0;
+
+// clickableImage.addEventListener("click", function() {
+//   clickCount++;
+//   if (clickCount === 10) {
+//     clickableImage.src = "egg2.png";
+//     clickableImage.alt = "New Image";
+//     clickableImage.removeEventListener("click", arguments.callee);
+//   }
+// });
